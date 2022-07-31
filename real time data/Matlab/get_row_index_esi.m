00e006ff1040aafd7abcfeb9c09332a50aef7bef
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2022-07-31 10:42:33 +0200

    merge functions

---
+++ real time data/Matlab/get_row_index_esi.m
@@ -0,0 +1,21 @@
+function index_row = get_row_index_esi(dates, releasedates, vintage, freq, dateformat)
+
+is_released = false;
+t = size(dates, 1) + 1; % start with latest observations (+ 1, then reduce in loop)
+while ~is_released
+    t = t-1;
+    if strcmp(freq, 'm')
+        ind_ym_release = find(releasedates.('year') == year(dates{t}, dateformat) & ...
+                                releasedates.('month') == month(dates{t}, dateformat));
+    elseif strcmp(freq, 'q')
+        % assumes that quarterly values are published in the first month of
+        % the quarter!
+        ind_ym_release = find(releasedates.('year') == str2double(dates{t}(1:4)) & ...
+                                releasedates.('month') == str2double(dates{t}(end))*3-2);
+    else
+        error('freq has to either be m or q')
+    end
+    is_released = datenum(releasedates.('release_date')(ind_ym_release)) <= ...
+                                                                        datenum(vintage);    
+end
+index_row = t;
