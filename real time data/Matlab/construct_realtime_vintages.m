fea61dda9d06dae22cac737ad1d81b037332b3b4
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2022-07-31 10:01:01 +0200

    add commented line of single vintage

--- real time data/Matlab/construct_realtime_vintages.m
+++ real time data/Matlab/construct_realtime_vintages.m
@@ -2,13 +2,7 @@ function out = construct_realtime_vintages(dir_root)
     dir_rawdata = [dir_root, '/raw data'];
     dir_vintages = [dir_root, '/vintages'];
     vintages = importdata('../../dates_vintages.txt');
-%     vintages = {
-%         '30-Jan-2022', '15-Feb-2022',...
-%         '28-Feb-2022', '15-Mar-2022',...
-%         '30-Mar-2022', '15-Apr-2022',...
-%         '30-Apr-2022', '15-May-2022',...
-%         '30-May-2022', '15-Jun-2022',...
-%         '30-Jun-2022'}; 
+%    vintages = {'30-Jan-2022'};
 
     for v = 1 : length(vintages)
         %dataset.data_ifo = f_load_ifo(vintages{v}) ;
@@ -25,7 +19,7 @@ function out = construct_realtime_vintages(dir_root)
         %toc
         dataset.vintagedate = vintages{v} ; 
         % save dataset to mat
-        %save([ dir_vintages, '/dataset_', num2str(year(vintages{v})), '_' num2str(month(vintages{v})), '_', num2str(day(vintages{v})), '.mat'],'dataset');
+        save([ dir_vintages, '/dataset_', num2str(year(vintages{v})), '_' num2str(month(vintages{v})), '_', num2str(day(vintages{v})), '.mat'],'dataset');
 
         clear dataset
     end
