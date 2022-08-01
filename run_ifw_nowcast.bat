c7735f26c52320eb3bb072f194795b1a6f8f6aad
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2022-08-01 19:08:05 +0200

    add estimation of models to batch script

--- run_ifw_nowcast.bat
+++ run_ifw_nowcast.bat
@@ -1,17 +1,25 @@
 @echo off
 
-set DIR_ROOT=C:/Users/Philipp/Desktop/Echtzeitdatensatz
+set DIR_ROOT=C:/Users/Philipp/Desktop
+set DIR_REALTIMEDATA=%DIR_ROOT%/Echtzeitdatensatz
+ECHO %DIR_REALTIMEDATA%
 ECHO Downloading data...
 cd "real time data\R\"
-Rscript --vanilla download_bbk_rtd.R "%DIR_ROOT%/raw data"
-Rscript --vanilla download_bbk_financial_data.R "%DIR_ROOT%/raw data"
-Rscript --vanilla download_esi.R "%DIR_ROOT%/raw data"
-Rscript --vanilla compile_rtd_gastgewerbe.R "%DIR_ROOT%/raw data"
-Rscript --vanilla compile_rtd_lkw_maut_index.R "%DIR_ROOT%/raw data"
+Rem Rscript --vanilla download_bbk_rtd.R "%DIR_REALTIMEDATA%/raw data"
+Rem Rscript --vanilla download_bbk_financial_data.R "%DIR_REALTIMEDATA%/raw data"
+Rem Rscript --vanilla download_esi.R "%DIR_REALTIMEDATA%/raw data"
+Rem Rscript --vanilla compile_rtd_gastgewerbe.R "%DIR_REALTIMEDATA%/raw data"
+Rem Rscript --vanilla compile_rtd_lkw_maut_index.R "%DIR_REALTIMEDATA%/raw data"
 
 ECHO Constructing real-time vintages...
 cd ..\Matlab
-matlab -noFigureWindows -batch "construct_realtime_vintages('%DIR_ROOT%').m"
+Rem matlab -noFigureWindows -batch "construct_realtime_vintages('%DIR_REALTIMEDATA%').m"
+
+ECHO Estimating models...
+cd ..\..\model\
+matlab -noFigureWindows -batch "compute_nowcasts('%DIR_ROOT%').m"
+
+
 
 cd ..\..
 cmd /k
