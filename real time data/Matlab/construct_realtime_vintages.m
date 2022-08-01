53a1a2eca7e620aa0bc432c706bc53d8375d8cdd
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2022-08-01 19:07:15 +0200

    uncomment non-survey data blocks

--- real time data/Matlab/construct_realtime_vintages.m
+++ real time data/Matlab/construct_realtime_vintages.m
@@ -1,23 +1,22 @@
-function out = construct_realtime_vintages(dir_root)
-    dir_rawdata = [dir_root, '/raw data'];
-    dir_vintages = [dir_root, '/vintages'];
+function out = construct_realtime_vintages(dir_realtimedata)
+    dir_rawdata = [dir_realtimedata, '/raw data'];
+    dir_vintages = [dir_realtimedata, '/vintages'];
     vintages = importdata('../../dates_vintages.txt');
-    %vintages = {'25-Jun-2022'};
 
     for v = 1 : length(vintages)
         dataset.data_ifo = f_load_ifo(vintages{v}, dir_rawdata) ;
         %toc
         dataset.data_ESIBCI = f_load_ESIBCI(vintages{v}, dir_rawdata) ;
         %toc
-        %dataset.data_BuBaRTD = f_load_BuBaRTD(vintages{v}, dir_rawdata) ;
+        dataset.data_BuBaRTD = f_load_BuBaRTD(vintages{v}, dir_rawdata) ;
         % toc
-        %dataset.data_BuBaRTD = f_load_turnover_hospitality(dataset.data_BuBaRTD, vintages{v}, dir_rawdata);
+        dataset.data_BuBaRTD = f_load_turnover_hospitality(dataset.data_BuBaRTD, vintages{v}, dir_rawdata);
         % toc
-        %dataset.data_BuBaRTD = f_load_lkw_maut_index(dataset.data_BuBaRTD, vintages{v}, dir_rawdata);
+        dataset.data_BuBaRTD = f_load_lkw_maut_index(dataset.data_BuBaRTD, vintages{v}, dir_rawdata);
         %toc
-        %dataset.data_financial = f_load_financial(vintages{v}, dir_rawdata) ;
+        dataset.data_financial = f_load_financial(vintages{v}, dir_rawdata) ;
         %toc
-        %dataset.vintagedate = vintages{v} ; 
+        dataset.vintagedate = vintages{v} ; 
         % save dataset to mat
         save([ dir_vintages, '/dataset_', num2str(year(vintages{v})), '_' num2str(month(vintages{v})), '_', num2str(day(vintages{v})), '.mat'],'dataset');
 
