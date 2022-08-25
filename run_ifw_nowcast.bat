6d5bfa60faa49a64c89f9ce350b382e5c7a6ffde
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2022-08-25 07:34:40 +0200

    prepare run for 2022Q3
    - uncomment data download section in batch file
    
    - update vintage dates in list

--- run_ifw_nowcast.bat
+++ run_ifw_nowcast.bat
@@ -9,11 +9,11 @@ set DIR_REALTIMEDATA=%DIR_ROOT%/Echtzeitdatensatz
 
 ECHO Downloading data...
 cd "real time data\R\"
-Rem Rscript --vanilla download_bbk_rtd.R "%DIR_REALTIMEDATA%/raw data"
-Rem Rscript --vanilla download_bbk_financial_data.R "%DIR_REALTIMEDATA%/raw data"
-Rem Rscript --vanilla download_esi.R "%DIR_REALTIMEDATA%/raw data"
-Rem Rscript --vanilla compile_rtd_gastgewerbe.R "%DIR_REALTIMEDATA%/raw data"
-Rem Rscript --vanilla compile_rtd_lkw_maut_index.R "%DIR_REALTIMEDATA%/raw data"
+Rscript --vanilla download_bbk_rtd.R "%DIR_REALTIMEDATA%/raw data"
+Rscript --vanilla download_bbk_financial_data.R "%DIR_REALTIMEDATA%/raw data"
+Rscript --vanilla download_esi.R "%DIR_REALTIMEDATA%/raw data"
+Rscript --vanilla compile_rtd_gastgewerbe.R "%DIR_REALTIMEDATA%/raw data"
+Rscript --vanilla compile_rtd_lkw_maut_index.R "%DIR_REALTIMEDATA%/raw data"
 
 ECHO Constructing real-time vintages...
 cd ..\Matlab
@@ -22,8 +22,6 @@ matlab -noFigureWindows -batch "construct_realtime_vintages('%DIR_REALTIMEDATA%'
 ECHO Estimating models...
 cd ..\..\model\
 matlab -noFigureWindows -batch "compute_nowcasts('%DIR_ROOT%', '%YEAR%', '%QUARTER%').m"
-
-
 
 cd ..
 cmd /k
