c1209e5eaff1cafd11c5e5aebb3e1ea9d35a5ea6
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2022-08-31 16:21:21 +0200

    update to run with latest esi data

--- model/compute_nowcasts.m
+++ model/compute_nowcasts.m
@@ -74,7 +74,7 @@ function out = compute_nowcasts(dir_root, year_nowcast, quarter_nowcast)
     %Njs = [0] ; % # of lags in idiosyncratic component
     
     % switches
-    switch_estimatemodels = 0; % 1 = yes!
+    switch_estimatemodels = 1; % 1 = yes!
     switch_savetables = 1; % 1 = yes!
     switch_savegraphs = 1 ; % 1 = yes!
     switch_savedocus = 1 ; % 1 = yes!
