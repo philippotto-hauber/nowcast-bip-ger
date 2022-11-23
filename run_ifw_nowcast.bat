be7b29ac9246f84ff93ad617ac3825010a22d144
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2022-11-23 17:49:59 +0100

    add switches for different sections
    - download data, construct vintages, estimate models

--- run_ifw_nowcast.bat
+++ run_ifw_nowcast.bat
@@ -7,21 +7,36 @@ set /p QUARTER="Quarter: "
 set DIR_ROOT=C:/Users/Philipp/Desktop
 set DIR_REALTIMEDATA=%DIR_ROOT%/Echtzeitdatensatz
 
-ECHO Downloading data...
-cd "real time data\R\"
-Rscript --vanilla download_bbk_rtd.R "%DIR_REALTIMEDATA%/raw data"
-Rscript --vanilla download_bbk_financial_data.R "%DIR_REALTIMEDATA%/raw data"
-Rscript --vanilla download_esi.R "%DIR_REALTIMEDATA%/raw data"
-Rscript --vanilla compile_rtd_gastgewerbe.R "%DIR_REALTIMEDATA%/raw data"
-Rscript --vanilla compile_rtd_lkw_maut_index.R "%DIR_REALTIMEDATA%/raw data"
 
-ECHO Constructing real-time vintages...
-cd ..\Matlab
-matlab -noFigureWindows -batch "construct_realtime_vintages('%DIR_REALTIMEDATA%').m"
+set /A switch_download_data = 1
+IF %switch_download_data%==1 (
+    ECHO Downloading data...
+    cd "real time data\R\"
+    Rscript --vanilla download_bbk_rtd.R "%DIR_REALTIMEDATA%/raw data"
+    Rscript --vanilla download_bbk_financial_data.R "%DIR_REALTIMEDATA%/raw data"
+    Rscript --vanilla download_esi.R "%DIR_REALTIMEDATA%/raw data"
+    Rscript --vanilla compile_rtd_gastgewerbe.R "%DIR_REALTIMEDATA%/raw data"
+    Rscript --vanilla compile_rtd_lkw_maut_index.R "%DIR_REALTIMEDATA%/raw data"
+) ELSE (
+    ECHO "Switch set to 0. Do not download data"
+)
 
-ECHO Estimating models...
-cd ..\..\model\
-matlab -noFigureWindows -batch "compute_nowcasts('%DIR_ROOT%', '%YEAR%', '%QUARTER%').m"
+set /A switch_construct_vintages = 0
+IF %switch_construct_vintages%==1 (
+    ECHO Constructing real-time vintages...    
+    cd ..\Matlab
+    matlab -noFigureWindows -batch "construct_realtime_vintages('%DIR_REALTIMEDATA%').m"
+) ELSE (
+    ECHO "Switch set to 0. Do not construct vintages"
+)
 
-cd ..
-cmd /k
+set /A switch_estimate_models = 0
+IF %switch_estimate_models%==1 (
+    ECHO Estimating models...
+    cd ..\..\model\
+    matlab -noFigureWindows -batch "compute_nowcasts('%DIR_ROOT%', '%YEAR%', '%QUARTER%').m"
+    cd ..
+    cmd /k
+) ELSE (
+    ECHO "Switch set to 0. Do not estimate models"
+)
