01d0df85ed22633f8dee148b65db8ff90ce5e4bc
Philipp Hauber <44675474+ottothemotto@users.noreply.github.com>
2022-11-28 08:35:29 +0100

    capitalize commands in batch script

--- run_ifw_nowcast.bat
+++ run_ifw_nowcast.bat
@@ -1,13 +1,13 @@
 @echo off
 ECHO "Set year and quarter of nowcast (also produces forecast for the next quarter)!"
-set /p YEAR="Year: "
-set /p QUARTER="Quarter: "
+SET /p YEAR="Year: "
+SET /p QUARTER="Quarter: "
 
-set DIR_ROOT=C:/Users/Philipp/Desktop
-set DIR_REALTIMEDATA=%DIR_ROOT%/Echtzeitdatensatz
+SET DIR_ROOT=C:/Users/Philipp/Desktop
+SET DIR_REALTIMEDATA=%DIR_ROOT%/Echtzeitdatensatz
 
-set /A switch_download_data = 0
-cd "real time data\R\"
+SET /A switch_download_data = 0
+CD "real time data\R\"
 IF %switch_download_data%==1 (
     ECHO Downloading data...
     Rscript --vanilla download_bbk_rtd.R "%DIR_REALTIMEDATA%/raw data"
@@ -19,8 +19,8 @@ IF %switch_download_data%==1 (
     ECHO "Switch set to 0. Do not download data"
 )
 
-set /A switch_construct_vintages = 0
-cd "..\Matlab"
+SET /A switch_construct_vintages = 0
+CD "..\Matlab"
 IF %switch_construct_vintages%==1 (
     ECHO Constructing real-time vintages...  
     matlab -noFigureWindows -batch "construct_realtime_vintages('%DIR_REALTIMEDATA%').m"
@@ -28,8 +28,8 @@ IF %switch_construct_vintages%==1 (
     ECHO "Switch set to 0. Do not construct vintages"
 )
 
-cd "..\..\model\"
-set /A switch_estimate_models = 1
+CD "..\..\model\"
+SET /A switch_estimate_models = 1
 IF %switch_estimate_models%==1 (
     ECHO Estimating models...
     matlab -noFigureWindows -batch "compute_nowcasts('%DIR_ROOT%', '%YEAR%', '%QUARTER%').m"
@@ -37,7 +37,7 @@ IF %switch_estimate_models%==1 (
     ECHO "Switch set to 0. Do not estimate models"
 )
 
-set /A switch_additional_plots = 1
+SET /A switch_additional_plots = 1
 IF %switch_additional_plots% == 1 (
     ECHO "Plotting monthly GDP and non-GDP forecasts"
     matlab -noFigureWindows -batch "plot_monthlyGDP('%DIR_ROOT%', '%YEAR%', '%QUARTER%').m"
@@ -45,5 +45,5 @@ IF %switch_additional_plots% == 1 (
 ) ELSE (
     ECHO "Switch set to 0. Do not produce additional plots"
 )
-cd ..
-cmd /k
+CD ..
+CMD /k
