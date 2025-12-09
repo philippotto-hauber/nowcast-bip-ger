3951fa04b84a3ba504c485a1ebea2702308cfcd9
Philipp Hauber <philipp.hauber@posteo.de>
2025-12-09 05:15:31 +0100

    set dir_repo in batch script

--- run-nowcast.bat
+++ run-nowcast.bat
@@ -3,18 +3,20 @@ ECHO "Set year and quarter of nowcast (also produces forecast for the next quart
 SET /p YEAR="Year: "
 SET /p QUARTER="Quarter: "
 
-SET DIR_ROOT=%CD%
-SET DIR_REALTIMEDATA=%DIR_ROOT%/Echtzeitdatensatz
+SET DIR_REPO=%CD%
+SET DIR_ROOT="C:\Users\Hauber-P\Documents"
+SET DIR_REALTIMEDATA=%DIR_ROOT%\Echtzeitdatensatz
 
 SET /A switch_download_data = 1
 CD "src\real time data\R\"
 IF %switch_download_data%==1 (
-    ECHO Downloading data...
+    ECHO Downloading data...    
     Rscript --vanilla download_bbk_rtd.R "%DIR_REALTIMEDATA%/raw data"
     Rscript --vanilla download_bbk_financial_data.R "%DIR_REALTIMEDATA%/raw data"
-    Rscript --vanilla download_esi.R "%DIR_REALTIMEDATA%/raw data"
-    Rscript --vanilla compile_rtd_gastgewerbe.R "%DIR_REALTIMEDATA%/raw data"
-    Rscript --vanilla compile_rtd_lkw_maut_index.R "%DIR_REALTIMEDATA%/raw data"
+    Rscript --vanilla download_esi.R "%DIR_REALTIMEDATA%/raw data"    
+    Rscript --vanilla download_ifo.R "%DIR_REALTIMEDATA%/raw data"
+    Rscript --vanilla compile_rtd_lkw_maut_index.R "%DIR_REALTIMEDATA%\raw data" "%DIR_REPO%"
+    Rscript --vanilla compile_rtd_gastgewerbe.R "%DIR_REALTIMEDATA%\raw data" "%DIR_REPO%"
 ) ELSE (
     ECHO "Switch set to 0. Do not download data"
 )
@@ -23,7 +25,7 @@ SET /A switch_construct_vintages = 1
 CD "..\Matlab"
 IF %switch_construct_vintages%==1 (
     ECHO Constructing real-time vintages...  
-    matlab -noFigureWindows -batch "construct_realtime_vintages('%DIR_REALTIMEDATA%').m"
+    matlab -noFigureWindows -batch "construct_realtime_vintages('%DIR_REALTIMEDATA%', '%DIR_REPO%').m"
 ) ELSE (
     ECHO "Switch set to 0. Do not construct vintages"
 )
@@ -38,7 +40,7 @@ IF %switch_compute_nowcasts%==1 (
     ECHO "Switch set to 0. Do not compute nowcasts"
 )
 
-SET /A switch_additional_plots = 0
+SET /A switch_additional_plots = 1
 IF %switch_additional_plots% == 1 (
     ECHO "Plotting monthly GDP and non-GDP forecasts"
     matlab -noFigureWindows -batch "plot_monthlyGDP('%DIR_ROOT%', '%YEAR%', '%QUARTER%').m"
