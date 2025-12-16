3c98f9a8b6f7c70a87e6c05cc18a941eb9638ab4
Philipp Hauber <philipp.hauber@posteo.de>
2025-12-16 15:24:15 +0100

    add ESI subgroup to variable name

--- src/real time data/Matlab/f_loadoptions_ESIBCI.m
+++ src/real time data/Matlab/f_loadoptions_ESIBCI.m
@@ -4,53 +4,53 @@ function data = f_loadoptions_ESIBCI
 % group field is overwritten in f_load_ESIBCI with 'surveys'!
 
         temp = {% -------INDUSTRY-------------- %
-        'Confidence Indicator' , 'ESI: industry' , 2 , 'm' , 1 , 0 , 'INDU.DE.TOT.COF.BS.M'  ;                           
-        'Production trend observed in recent months' , 'ESI: industry' , 2 ,'m' , 1 , 0 , 'INDU.DE.TOT.1.BS.M' ;                  
-        'Assessment of order-book levels' , 'ESI: industry', 2 , 'm' , 1 , 0 , 'INDU.DE.TOT.2.BS.M' ;                            
-        'Assessment of export order-book levels' , 'ESI: industry' , 2 , 'm' , 1 , 0 , 'INDU.DE.TOT.3.BS.M' ;                       
-        'Assessment of stocks of finished products' , 'ESI: industry', 2 , 'm' ,1 , 0 ,'INDU.DE.TOT.4.BS.M' ;                     
-        'Production expectations for the months ahead' , 'ESI: industry', 2 ,'m' , 1 , 1 , 'INDU.DE.TOT.5.BS.M' ;                  
-        'Selling price expectations for the months ahead' , 'ESI: industry' , 2 , 'm' , 1 , 1 , 'INDU.DE.TOT.6.BS.M' ;             
-        'Employment expectations for the months ahead' , 'ESI: industry' , 2 , 'm' , 1 , 0 , 'INDU.DE.TOT.7.BS.M' ;
+        'Industry: Confidence Indicator' , 'ESI: industry' , 2 , 'm' , 1 , 0 , 'INDU.DE.TOT.COF.BS.M'  ;                           
+        'Industry: Production trend observed in recent months' , 'ESI: industry' , 2 ,'m' , 1 , 0 , 'INDU.DE.TOT.1.BS.M' ;                  
+        'Industry: Assessment of order-book levels' , 'ESI: industry', 2 , 'm' , 1 , 0 , 'INDU.DE.TOT.2.BS.M' ;                            
+        'Industry: Assessment of export order-book levels' , 'ESI: industry' , 2 , 'm' , 1 , 0 , 'INDU.DE.TOT.3.BS.M' ;                       
+        'Industry: Assessment of stocks of finished products' , 'ESI: industry', 2 , 'm' ,1 , 0 ,'INDU.DE.TOT.4.BS.M' ;                     
+        'Industry: Production expectations for the months ahead' , 'ESI: industry', 2 ,'m' , 1 , 1 , 'INDU.DE.TOT.5.BS.M' ;                  
+        'Industry: Selling price expectations for the months ahead' , 'ESI: industry' , 2 , 'm' , 1 , 1 , 'INDU.DE.TOT.6.BS.M' ;             
+        'Industry: Employment expectations for the months ahead' , 'ESI: industry' , 2 , 'm' , 1 , 0 , 'INDU.DE.TOT.7.BS.M' ;
         %'Export expectations for the months ahead' ,   'ESI: industry' , 2 , 'q:A' , 0 , 0 , 'INDU.DE.TOT.12.BS.Q' ;                      
-        'Current level of capacity utilization'  'ESI: industry' , 2 , 'q:A' , 0 , 0 , 'INDU.DE.TOT.13.QPS.Q' ; 
+        'Industry: Current level of capacity utilization'  'ESI: industry' , 2 , 'q:A' , 0 , 0 , 'INDU.DE.TOT.13.QPS.Q' ; 
         % -------RETAIL-------------- %
-        'Confidence Indicator' , 'ESI: retail' , 2 , 'm' , 1 , 0 , 'RETA.DE.TOT.COF.BS.M' ;                             
-        'Business activity (sales) development over the past 3 months' , 'ESI: retail' , 2 , 'm' , 1 , 0 , 'RETA.DE.TOT.1.BS.M' ;
-        'Volume of stock currently hold' , 'ESI: retail' , 2 , 'm' , 1 , 0 , 'RETA.DE.TOT.2.BS.M' ; 
-        'Orders expectations over the next 3 months' , 'ESI: retail' , 2 , 'm' , 1 , 0 , 'RETA.DE.TOT.3.BS.M' ; 
-        'Business activity expectations over the next 3 months' , 'ESI: retail' , 2 , 'm' , 1 , 0 , 'RETA.DE.TOT.4.BS.M' ; 
-        'Employment expectations over the next 3 months' , 'ESI: retail' , 2 , 'm' , 1 , 0 , 'RETA.DE.TOT.5.BS.M' ; 
-        'Prices expectations over the next 3 months' , 'ESI: retail' , 2 , 'm' , 1 , 1 , 'RETA.DE.TOT.6.BS.M' ;
+        'Retail: Confidence Indicator' , 'ESI: retail' , 2 , 'm' , 1 , 0 , 'RETA.DE.TOT.COF.BS.M' ;                             
+        'Retail: Business activity (sales) development over the past 3 months' , 'ESI: retail' , 2 , 'm' , 1 , 0 , 'RETA.DE.TOT.1.BS.M' ;
+        'Retail: Volume of stock currently hold' , 'ESI: retail' , 2 , 'm' , 1 , 0 , 'RETA.DE.TOT.2.BS.M' ; 
+        'Retail: Orders expectations over the next 3 months' , 'ESI: retail' , 2 , 'm' , 1 , 0 , 'RETA.DE.TOT.3.BS.M' ; 
+        'Retail: Business activity expectations over the next 3 months' , 'ESI: retail' , 2 , 'm' , 1 , 0 , 'RETA.DE.TOT.4.BS.M' ; 
+        'Retail: Employment expectations over the next 3 months' , 'ESI: retail' , 2 , 'm' , 1 , 0 , 'RETA.DE.TOT.5.BS.M' ; 
+        'Retail: Prices expectations over the next 3 months' , 'ESI: retail' , 2 , 'm' , 1 , 1 , 'RETA.DE.TOT.6.BS.M' ;
         % -------CONSTRUCTION-------------- %
-        'Confidence Indicator' , 'ESI: building' , 2 , 'm' , 1 , 1 , 'BUIL.DE.TOT.COF.BS.M' ;                               
-        'Building activity development over the past 3 months' , 'ESI: building' , 2 , 'm' , 1 , 1 , 'BUIL.DE.TOT.1.BS.M' ; 
-        'Evolution of your current overall order books' , 'ESI: building' , 2 , 'm' , 1 , 1 , 'BUIL.DE.TOT.3.BS.M' ;
-        'Employment expectations over the next 3 months' , 'ESI: building' , 2 , 'm' , 1 , 1 , 'BUIL.DE.TOT.4.BS.M' ; 
-        'Prices expectations over the next 3 months' , 'ESI: building' , 2 , 'm' , 1 , 1 , 'BUIL.DE.TOT.5.BS.M' ; 
+        'Construction: Confidence Indicator' , 'ESI: building' , 2 , 'm' , 1 , 1 , 'BUIL.DE.TOT.COF.BS.M' ;                               
+        'Construction: Building activity development over the past 3 months' , 'ESI: building' , 2 , 'm' , 1 , 1 , 'BUIL.DE.TOT.1.BS.M' ; 
+        'Construction: Evolution of your current overall order books' , 'ESI: building' , 2 , 'm' , 1 , 1 , 'BUIL.DE.TOT.3.BS.M' ;
+        'Construction: Employment expectations over the next 3 months' , 'ESI: building' , 2 , 'm' , 1 , 1 , 'BUIL.DE.TOT.4.BS.M' ; 
+        'Construction: Prices expectations over the next 3 months' , 'ESI: building' , 2 , 'm' , 1 , 1 , 'BUIL.DE.TOT.5.BS.M' ; 
         % -------SERVICES-------------- %
-        'Confidence Indicator' , 'ESI: services' , 2 , 'm' , 0 , 0 , 'SERV.DE.TOT.COF.BS.M' ;                            
-        'Business situation development over the past 3 months' , 'ESI: services' , 2 , 'm' , 0 , 0 , 'SERV.DE.TOT.1.BS.M' ;
-        'Evolution of the demand over the past 3 months' , 'ESI: services' , 2 , 'm' , 0 , 0 , 'SERV.DE.TOT.2.BS.M' ; 
-        'Expectation of the demand over the next 3 months' , 'ESI: services' , 2 , 'm' , 0 , 1 , 'SERV.DE.TOT.3.BS.M' ; 
+        'Services: Confidence Indicator' , 'ESI: services' , 2 , 'm' , 0 , 0 , 'SERV.DE.TOT.COF.BS.M' ;                            
+        'Services: Business situation development over the past 3 months' , 'ESI: services' , 2 , 'm' , 0 , 0 , 'SERV.DE.TOT.1.BS.M' ;
+        'Services: Evolution of the demand over the past 3 months' , 'ESI: services' , 2 , 'm' , 0 , 0 , 'SERV.DE.TOT.2.BS.M' ; 
+        'Services: Expectation of the demand over the next 3 months' , 'ESI: services' , 2 , 'm' , 0 , 1 , 'SERV.DE.TOT.3.BS.M' ; 
         %'Evolution of the employment over the past 3 months' , 'ESI: services' , 2 , 'm' , 0 , 0 , 'SERV.DE.TOT.4.BS.M' ; 
-        'Expectations of the employment over the next 3 months' , 'ESI: services' , 2 , 'm' , 0 , 1 , 'SERV.DE.TOT.5.BS.M' ; 
-        'Expectations of the prices over the next 3 months' , 'ESI: services' , 2 , 'm' , 0 , 1 , 'SERV.DE.TOT.6.BS.M'
-        'Current level of capacity utilization' , 'ESI: services' , 2 , 'q:A' , 0 , 0 , 'SERV.DE.TOT.8.QPS.Q' ;     
+        'Services: Expectations of the employment over the next 3 months' , 'ESI: services' , 2 , 'm' , 0 , 1 , 'SERV.DE.TOT.5.BS.M' ; 
+        'Services: Expectations of the prices over the next 3 months' , 'ESI: services' , 2 , 'm' , 0 , 1 , 'SERV.DE.TOT.6.BS.M'
+        'Services: Current level of capacity utilization' , 'ESI: services' , 2 , 'q:A' , 0 , 0 , 'SERV.DE.TOT.8.QPS.Q' ;     
         % -------CONSUMER-------------- %
-        'Confidence Indicator' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.COF.BS.M' ;                            
-        'Financial situation over last 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.1.BS.M' ; 
-        'Financial situation over next 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.2.BS.M' ;
-        'General economic situation over last 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.3.BS.M' ;
-        'General economic situation over next 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.4.BS.M' ;
-        'Price trends over last 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.5.BS.M' ;
-        'Price trends over next 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.6.BS.M' ;
-        'Unemployment expectations over next 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.7.BS.M' ;
-        'Major purchases at present' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.8.BS.M' ;
-        'Major purchases over next 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.9.BS.M' ;
+        'Consumer: Confidence Indicator' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.COF.BS.M' ;                            
+        'Consumer: Financial situation over last 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.1.BS.M' ; 
+        'Consumer: Financial situation over next 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.2.BS.M' ;
+        'Consumer: General economic situation over last 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.3.BS.M' ;
+        'Consumer: General economic situation over next 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.4.BS.M' ;
+        'Consumer: Price trends over last 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.5.BS.M' ;
+        'Consumer: Price trends over next 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.6.BS.M' ;
+        'Consumer: Unemployment expectations over next 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.7.BS.M' ;
+        'Consumer: Major purchases at present' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.8.BS.M' ;
+        'Consumer: Major purchases over next 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.9.BS.M' ;
         %'Savings at present' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.10.BS.M' ;
-        'Savings over next 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.11.BS.M' ;
-        'Statement on financial situation of household' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.12.BS.M' ; 
+        'Consumer: Savings over next 12 months' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.11.BS.M' ;
+        'Consumer: Statement on financial situation of household' , 'ESI: consumer' , 2 , 'm' , 1 , 0 , 'CONS.DE.TOT.12.BS.M' ; 
         } ;
             
 data.names = temp(:,1)' ;         
