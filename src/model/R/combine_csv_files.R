fdd8fa9055807d819d74acc380f69d54d8db859e
Philipp Hauber <philipp.hauber@posteo.de>
2025-12-20 08:06:20 +0100

    rename file

---
+++ src/model/R/combine_csv_files.R
@@ -0,0 +1,23 @@
+args <- commandArgs(trailingOnly = TRUE)
+dir_root <- args[1]
+nowcast_year <- args[2]
+nowcast_quarter <- args[3]
+# for debugging
+# dir_root <- "C:/Users/Hauber-P/Documents/dev"
+# nowcast_year <- 2025
+# nowcast_quarter <- 4
+
+dir_nowcast <- paste0(dir_root, "/Nowcasts/", nowcast_year, "Q", nowcast_quarter, "/output_csv")
+patterns <- c("forecasts", "news")
+
+for (p in patterns){
+    files <- list.files(path = dir_nowcast, pattern = paste0("_", p, "_"), full.names = TRUE)
+    print(files)
+    dat_out <- do.call(
+        rbind,
+        lapply(files, read.csv)
+    )
+
+    write.csv(dat_out, paste0(dir_nowcast, "/", p, ".csv"), row.names = FALSE)
+    file.remove(files)
+}
