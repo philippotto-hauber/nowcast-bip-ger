d7999b907485e7f5fa85d458d13f3e51d921848a
Philipp Hauber <philipp.hauber@posteo.de>
2026-01-07 11:22:46 +0100

    update batch script
    call all files from project root dir so that renv gets activated

--- run-nowcast.bat
+++ run-nowcast.bat
@@ -8,41 +8,41 @@ SET DIR_ROOT="C:\Users\Hauber-P\Documents\dev"
 SET DIR_REALTIMEDATA=%DIR_ROOT%\Echtzeitdatensatz
 
 SET /A switch_download_data = 0
-CD "src\real time data\R\"
+Rem CD "src\real time data\R\"
 IF %switch_download_data%==1 (
     ECHO Downloading data...    
-    Rscript --vanilla download_bbk_rtd.R "%DIR_REALTIMEDATA%/raw data"
-    Rscript --vanilla download_bbk_financial_data.R "%DIR_REALTIMEDATA%/raw data"
-    Rscript --vanilla download_esi.R "%DIR_REALTIMEDATA%/raw data"    
-    Rscript --vanilla download_ifo.R "%DIR_REALTIMEDATA%/raw data"
-    Rscript --vanilla compile_rtd_lkw_maut_index.R "%DIR_REALTIMEDATA%\raw data" "%DIR_REPO%"
-    Rscript --vanilla compile_rtd_gastgewerbe.R "%DIR_REALTIMEDATA%\raw data" "%DIR_REPO%"
-    Rscript --vanilla postprocess_Bbk_vintages.R "%DIR_REALTIMEDATA%/raw data"
+    Rscript --no-save --no-restore "./src/real time data/R/download_bbk_rtd.R" "%DIR_REALTIMEDATA%/raw data"
+    Rscript --no-save --no-restore "./src/real time data/R/download_bbk_financial_data.R" "%DIR_REALTIMEDATA%/raw data"
+    Rscript --no-save --no-restore "./src/real time data/R/download_esi.R" "%DIR_REALTIMEDATA%/raw data"    
+    Rscript --no-save --no-restore "./src/real time data/R/download_ifo.R" "%DIR_REALTIMEDATA%/raw data"
+    Rscript --no-save --no-restore "./src/real time data/R/compile_rtd_lkw_maut_index.R" "%DIR_REALTIMEDATA%\raw data" "%DIR_REPO%"
+    Rscript --no-save --no-restore "./src/real time data/R/compile_rtd_gastgewerbe.R" "%DIR_REALTIMEDATA%\raw data" "%DIR_REPO%"
+    Rscript --no-save --no-restore "./src/real time data/R/postprocess_Bbk_vintages.R" "%DIR_REALTIMEDATA%/raw data"
 ) ELSE (
     ECHO "Switch set to 0. Do not download data"
 )
 
 SET /A switch_construct_vintages = 0
-CD "..\Matlab"
+Rem CD "..\Matlab"
 IF %switch_construct_vintages%==1 (
     ECHO Constructing real-time vintages...     
-    matlab -noFigureWindows -batch "construct_realtime_vintages('%DIR_REALTIMEDATA%', '%DIR_REPO%').m" 
-    CD "..\R"
-    Rscript --vanilla convert_mat_vintages_to_csv.R "%DIR_REALTIMEDATA%/vintages"   
+    matlab -noFigureWindows -batch "addpath('./src/model/Matlab'); construct_realtime_vintages('%DIR_REALTIMEDATA%', '%DIR_REPO%').m" 
+    Rem CD "..\R"
+    Rscript --no-save --no-restore "./src/real time data/R/convert_mat_vintages_to_csv.R "%DIR_REALTIMEDATA%/vintages"   
 ) ELSE (
     ECHO "Switch set to 0. Do not construct vintages"
 )
 
-CD "..\..\.."
+Rem CD "..\..\.."
 SET /A switch_compute_nowcasts = 1
 SET /A switch_estimate_models = 0
 IF %switch_compute_nowcasts%==1 (
     ECHO Computing nowcasts...
-    Rem matlab -noFigureWindows -batch "compute_nowcasts('%DIR_ROOT%', '%YEAR%', '%QUARTER%', '%switch_estimate_models%').m"
-    Rem matlab -noFigureWindows -batch "plot_nowcast_evolution('%DIR_ROOT%', '%YEAR%', '%QUARTER%').m"
-    Rem matlab -noFigureWindows -batch "print_news_docu('%DIR_ROOT%', '%YEAR%', '%QUARTER%').m"
+    matlab -noFigureWindows -batch "addpath('./src/model/Matlab'); addpath('./src/model/Matlab/functions');compute_nowcasts('%DIR_ROOT%', '%YEAR%', '%QUARTER%', '%switch_estimate_models%').m"
+    Rem matlab -noFigureWindows -batch "addpath('./src/model/Matlab'); plot_nowcast_evolution('%DIR_ROOT%', '%YEAR%', '%QUARTER%').m"
+    matlab -noFigureWindows -batch "addpath('./src/model/Matlab'); addpath('./src/model/Matlab/functions'); print_news_docu('%DIR_ROOT%', '%YEAR%', '%QUARTER%').m"
     Rem CD "..\R"
-    Rem Rscript --vanilla combine_csv_files.R "%DIR_ROOT%" "%YEAR%" "%QUARTER%"
+    Rscript --no-save --no-restore "./src/model/R/combine_csv_files.R "%DIR_ROOT%" "%YEAR%" "%QUARTER%"
     Rscript --no-save --no-restore "./src/model/R/gen_plts.R" "%DIR_ROOT%" "%YEAR%" "%QUARTER%"
 ) ELSE (
     ECHO "Switch set to 0. Do not compute nowcasts"
