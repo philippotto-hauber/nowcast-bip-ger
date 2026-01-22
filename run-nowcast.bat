0c31b46c95a52dc1812a96a6a284b83a16360ca7
Philipp Hauber <philipp.hauber@posteo.de>
2026-01-22 11:18:28 +0100

    update script, mv and add to batch

--- run-nowcast.bat
+++ run-nowcast.bat
@@ -7,15 +7,15 @@ SET DIR_REPO=%CD%
 SET DIR_ROOT="C:\Users\Hauber-P\Documents\dev"
 SET DIR_REALTIMEDATA=%DIR_ROOT%\Echtzeitdatensatz
 
-SET /A switch_download_data = 1
+SET /A switch_download_data = 0
 IF %switch_download_data%==1 (
     ECHO Downloading data...    
     Rscript --no-save --no-restore "./src/real time data/R/download_bbk_rtd.R" "%DIR_REALTIMEDATA%/raw data"
-    Rem Rscript --no-save --no-restore "./src/real time data/R/download_bbk_financial_data.R" "%DIR_REALTIMEDATA%/raw data"
-    Rem Rscript --no-save --no-restore "./src/real time data/R/download_esi.R" "%DIR_REALTIMEDATA%/raw data"    
-    Rem Rscript --no-save --no-restore "./src/real time data/R/download_ifo.R" "%DIR_REALTIMEDATA%/raw data"
-    Rem Rscript --no-save --no-restore "./src/real time data/R/compile_rtd_lkw_maut_index.R" "%DIR_REALTIMEDATA%\raw data" "%DIR_REPO%"
-    Rem Rscript --no-save --no-restore "./src/real time data/R/compile_rtd_gastgewerbe.R" "%DIR_REALTIMEDATA%\raw data" "%DIR_REPO%"
+    Rscript --no-save --no-restore "./src/real time data/R/download_bbk_financial_data.R" "%DIR_REALTIMEDATA%/raw data"
+    Rscript --no-save --no-restore "./src/real time data/R/download_esi.R" "%DIR_REALTIMEDATA%/raw data"    
+    Rscript --no-save --no-restore "./src/real time data/R/download_ifo.R" "%DIR_REALTIMEDATA%/raw data"
+    Rscript --no-save --no-restore "./src/real time data/R/compile_rtd_lkw_maut_index.R" "%DIR_REALTIMEDATA%\raw data" "%DIR_REPO%"
+    Rscript --no-save --no-restore "./src/real time data/R/compile_rtd_gastgewerbe.R" "%DIR_REALTIMEDATA%\raw data" "%DIR_REPO%"
     Rscript --no-save --no-restore "./src/real time data/R/postprocess_Bbk_vintages.R" "%DIR_REALTIMEDATA%/raw data"
 ) ELSE (
     ECHO "Switch set to 0. Do not download data"
@@ -31,14 +31,14 @@ IF %switch_construct_vintages%==1 (
 )
 
 SET /A switch_compute_nowcasts = 1
-SET /A switch_estimate_models = 1
+SET /A switch_estimate_models = 0
 IF %switch_compute_nowcasts%==1 (
     ECHO Computing nowcasts...
     matlab -noFigureWindows -batch "addpath('./src/model/Matlab'); addpath('./src/model/Matlab/functions');compute_nowcasts('%DIR_ROOT%', '%YEAR%', '%QUARTER%', '%switch_estimate_models%').m"
-    matlab -noFigureWindows -batch "addpath('./src/model/Matlab'); addpath('./src/model/Matlab/functions'); print_news_docu('%DIR_ROOT%', '%YEAR%', '%QUARTER%').m"
     Rscript --no-save --no-restore "./src/model/R/combine_csv_files.R" "%DIR_ROOT%" "%YEAR%" "%QUARTER%"
     Rscript --no-save --no-restore "./src/model/R/calculate_bottom_up_forecast.R" "%DIR_ROOT%" "%YEAR%" "%QUARTER%"
     Rscript --no-save --no-restore "./src/model/R/gen_plts.R" "%DIR_ROOT%" "%YEAR%" "%QUARTER%"
+    Rscript --no-save --no-restore "./src/model/R/gen_table_news_decomp.R" "%DIR_ROOT%" "%YEAR%" "%QUARTER%"
 ) ELSE (
     ECHO "Switch set to 0. Do not compute nowcasts"
 )
---
+++ src/model/R/gen_table_news_decomp.R
@@ -0,0 +1,124 @@
+# args <- commandArgs(trailingOnly = TRUE)
+# dir_root <- args[1]
+# nowcast_year <- args[2]
+# nowcast_quarter <- args[3]
+# for debugging
+dir_root <- "C:/Users/Hauber-P/Documents/dev"
+nowcast_year <- 2025
+nowcast_quarter <- 4
+
+# setup ----
+library(collapse)
+library(ggplot2)
+library(gt)
+
+vars <- data.frame(
+  name = c("gross domestic product", "private consumption", "private gross fixed capital formation", "exports"),
+  mnemonic = c("Y", "C", "I", "X")
+)
+
+dir_tables <- paste0(dir_root, "/Nowcasts/", nowcast_year, "Q", nowcast_quarter, "/tables/")
+if (!dir.exists(paste0(dir_tables, "/models"))) dir.create(paste0(dir_tables, "/models"), recursive = TRUE)
+
+gen_table_news_decomp <- function(dat, threshold, str_target, str_period, str_model){
+  vintages_all <- sort(unique(dat$vintage))
+  dat <- dat |> 
+  collapse::fmutate(abs_val = abs(impact)) |> 
+  collapse::fsubset(abs_val >= threshold) |>  
+  collapse::roworder(-abs_val) |> 
+  collapse::fselect(vintage, group, variable, forecast, actual, weight, impact)
+  
+  vintages <- sort(unique(dat$vintage))
+
+  str_footnote <- paste0("Variable releases with an absolute impact smaller than ", threshold, " have been dropped. There were no releases with an absolute impact larger than for the following vintages: ", as.Date(setdiff(vintages_all, vintages)))
+
+  t <- dat |> 
+  gt::gt(groupname_col = "vintage") |>
+  row_group_order(groups = rev(as.character(vintages))) |> 
+  gt::tab_header(
+    title = paste0("News decomposition: ", str_target, " forecasts for ", str_period),
+    subtitle = glue::glue("model: ", str_model)
+  ) |>
+  gt::fmt_number(
+    columns = c("forecast", "actual"),
+    decimals = 2
+  ) |> 
+  gt::fmt_number(
+    columns = c("weight", "impact"),
+    decimals = 4
+  ) |> 
+  gt::tab_spanner(
+    label = "impact = (forecast - actual) x weight",
+    columns = c(forecast, actual, weight, impact)
+  ) |> 
+  gt::tab_style(
+    style = list(
+      cell_fill(color = "#C6EFCE"),
+      cell_text(color = "#006100", weight = "bold")
+    ),
+    locations = cells_body(
+      columns = impact,
+      rows = impact > 0
+    )
+  ) |> 
+  gt::tab_style(
+    style = list(
+      cell_fill(color = "#FFC7CE"),
+      cell_text(color = "#9C0006", weight = "bold")
+    ),
+    locations = cells_body(
+      columns = impact,
+      rows = impact < 0
+    )
+  ) |> 
+  tab_footnote(
+    footnote = str_footnote,
+    location = cells_title(groups = "title")
+  ) 
+  return(t)
+}
+
+# load data ----
+df_news <- read.csv(paste0(dir_root, "/Nowcasts/", nowcast_year, "Q", nowcast_quarter, "/output_csv/news.csv"))
+df_news$vintage <- readr::parse_date(df_news$vintage, locale = readr::locale("en"))
+
+# calculate equal-weight pool ----
+df_ew <- df_news |> 
+  fgroup_by(vintage, period, target, variable, group) |> 
+  fsummarise(
+    forecast = mean(forecast),
+    actual = mean(actual),
+    weight = mean(weight),
+    impact = mean(impact)
+  ) |> 
+  fmutate(model = "equal-weight pool")
+
+df_news <- rbind(df_news, df_ew)
+
+# loop to generate and save tables ----
+models <- unique(df_news$model) 
+targets <- unique(df_news$target)
+periods <- unique(df_news$period)
+
+for (t in targets){
+  for (p in periods){
+    for (m in models){
+      gen_table_news_decomp(
+        df_news |> 
+          fsubset(target == t & period == p & model == m),
+        threshold = 0.01,
+        str_target = t, 
+        str_period = p, 
+        str_model = m
+      ) |> 
+      gtsave(
+        ifelse(
+          m == "equal-weight pool", 
+          paste0(dir_tables, vars$mnemonic[which(vars$name == t)], "_", p, "_", m, ".html"),
+          paste0(dir_tables, "models/", vars$mnemonic[which(vars$name == t)], "_", p, "_", m, ".html")
+        )
+      )
+
+    }
+  }
+}
